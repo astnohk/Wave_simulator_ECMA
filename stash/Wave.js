d7bc71544359dcd8c896308c296aa559728d7970
sh-konta <kon.bass@gmail.com>
2016-08-21 22:21:54 +0900

    Change the line color with its tensions

--- stash/Wave.js
+++ stash/Wave.js
@@ -11,7 +11,7 @@ var braneSize = {width: 33, height: 33};
 var brane = new Array();
 var brane_tmp = new Array();
 var vel = new Array();
-var k_brane = 0.7;
+var k_brane = 0.2;
 var interval = 30; // drawing interval pixels
 
 var rainThreshold = 0.1;
@@ -19,6 +19,8 @@ var rainThreshold = 0.1;
 var field_XYZ = {X: {x: 1.0, y: 0.0, z: 0.0}, Y: {x: 0.0, y: 1.0, z: 0.0}, Z: {x: 0.0, y: 0.0, z: 1.0}};
 var offset = {x: 0, y: 0, z: 0};
 var rot_degree = 3600;
+var colormap_quantize = 200;
+var colormap = new Array();
 
 var prev_clientX = 0;
 var prev_clientY = 0;
@@ -34,6 +36,9 @@ window.addEventListener("load", init, false);
 function
 init()
 {
+	// Make colormap
+	makeColormap();
+	// Initialize brane
 	for (var i = 0; i < braneSize.height; i++) {
 		for (var j = 0; j < braneSize.width; j++) {
 			brane[i * braneSize.width + j] = 0.0;
@@ -41,12 +46,17 @@ init()
 			vel[i * braneSize.width + j] = 0.0;
 		}
 	}
+	// Set view offset
 	offset.x = braneSize.width * interval / 2.0
 	offset.y = braneSize.height * interval / 2.0
+	// Initialize canvas
 	canvas = document.getElementById("mainPool");
 	canvas.addEventListener("mousedown", mouseClick, false);
 	canvas.addEventListener("mousemove", mouseRotation, false);
+	canvas.addEventListener("touchstart", mouseClick, false);
+	canvas.addEventListener("touchmove", mouseRotation, false);
 	context = canvas.getContext("2d");
+	// Adjust initial view rotation
 	rot_field_XYZ(0, 1350);
 	// Start loop
 	timeClock = setInterval(loop, 25);
@@ -119,45 +129,62 @@ accel(i, j)
 }
 
 function
+makeColormap()
+{
+	var dc = Math.ceil(255 / (colormap_quantize / 2));
+	var i;
+	for (i = 0; i <= Math.floor(colormap_quantize / 2); i++) {
+		colormap[i] = 'rgb(0,' + Math.min(255, dc * i) + ',' + Math.max(0, 255 - dc * i) + ')';
+	}
+	for (i = Math.floor(colormap_quantize / 2); i < colormap_quantize; i++) {
+		colormap[i] = 'rgb(' + Math.min(255, dc * i) + ',' + Math.max(0, 255 - dc * i) + ',0)';
+	}
+}
+
+function
 draw()
 {
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	var xy = {x: 0, y: 0};
 	var i;
 	var j;
+	var amp;
+	context.strokeStyle = 'blue';
 	for (i = 0; i < braneSize.height; i++) {
-		context.beginPath();
-		context.strokeStyle = 'blue';
-		xy = calc_view(
-		    -offset.x,
-		    i * interval - offset.y,
-		    brane[i * braneSize.width]);
-		context.moveTo(xy.x + offset.x, xy.y + offset.y);
 		for (j = 1; j < braneSize.width; j++) {
+			amp = Math.round(2 * Math.max(Math.abs(brane[i * braneSize.width + j - 1]), Math.abs(brane[i * braneSize.width + j])));
+			context.strokeStyle = colormap[Math.min(colormap_quantize, amp)];
+			context.beginPath();
+			xy = calc_view(
+			    (j - 1) * interval - offset.x,
+			    i * interval - offset.y,
+			    brane[i * braneSize.width + j - 1]);
+			context.moveTo(xy.x + offset.x, xy.y + offset.y);
 			xy = calc_view(
 			    j * interval - offset.x,
 			    i * interval - offset.y,
 			    brane[i * braneSize.width + j]);
 			context.lineTo(xy.x + offset.x, xy.y + offset.y);
+			context.stroke();
 		}
-		context.stroke();
 	}
 	for (j = 0; j < braneSize.width; j++) {
-		context.beginPath();
-		context.strokeStyle = 'blue';
-		xy = calc_view(
-		    j * interval - offset.x,
-		    -offset.y,
-		    brane[j]);
-		context.moveTo(xy.x + offset.x, xy.y + offset.y);
 		for (i = 1; i < braneSize.height; i++) {
+			amp = Math.round(2 * Math.max(Math.abs(brane[(i - 1) * braneSize.width + j]), Math.abs(brane[i * braneSize.width + j])));
+			context.strokeStyle = colormap[Math.min(colormap_quantize, amp)];
+			context.beginPath();
+			xy = calc_view(
+			    j * interval - offset.x,
+			    (i - 1) * interval - offset.y,
+			    brane[(i - 1) * braneSize.width + j]);
+			context.moveTo(xy.x + offset.x, xy.y + offset.y);
 			xy = calc_view(
 			    j * interval - offset.x,
 			    i * interval - offset.y,
 			    brane[i * braneSize.width + j]);
 			context.lineTo(xy.x + offset.x, xy.y + offset.y);
+			context.stroke();
 		}
-		context.stroke();
 	}
 	// Show XYZ coordinate
 	context.lineWidth = 2;
@@ -237,12 +264,21 @@ rot_field_XYZ_onZ(x, y)
 	Y = field_XYZ.Y;
 	var cos = Math.cos(2.0 * Math.PI * x / rot_degree);
 	var sin = Math.sin(2.0 * Math.PI * x / rot_degree);
-	field_XYZ.X.x = X.x * cos - Y.x * sin;
-	field_XYZ.X.y = X.y * cos - Y.y * sin;
-	field_XYZ.X.z = X.z * cos - Y.z * sin;
-	field_XYZ.Y.x = Y.x * cos + X.x * sin;
-	field_XYZ.Y.y = Y.y * cos + X.y * sin;
-	field_XYZ.Y.z = Y.z * cos + X.z * sin;
+	if (field_XYZ.Z.y < 0.0) {
+		field_XYZ.X.x = X.x * cos + Y.x * sin;
+		field_XYZ.X.y = X.y * cos + Y.y * sin;
+		field_XYZ.X.z = X.z * cos + Y.z * sin;
+		field_XYZ.Y.x = Y.x * cos - X.x * sin;
+		field_XYZ.Y.y = Y.y * cos - X.y * sin;
+		field_XYZ.Y.z = Y.z * cos - X.z * sin;
+	} else {
+		field_XYZ.X.x = X.x * cos - Y.x * sin;
+		field_XYZ.X.y = X.y * cos - Y.y * sin;
+		field_XYZ.X.z = X.z * cos - Y.z * sin;
+		field_XYZ.Y.x = Y.x * cos + X.x * sin;
+		field_XYZ.Y.y = Y.y * cos + X.y * sin;
+		field_XYZ.Y.z = Y.z * cos + X.z * sin;
+	}
 	// normalize
 	var norm = norm_XYZ(field_XYZ.X);
 	if (norm > 0.1) {
@@ -260,16 +296,23 @@ mouseClick(event)
 	if (event.type === "mousedown") {
 		prev_clientX = event.clientX;
 		prev_clientY = event.clientY;
+	} else if (event.type === "touchstart") {
+		prev_clientX = event.touches[0].clientX;
+		prev_clientY = event.touches[0].clientY;
 	}
 }
 
 function
 mouseRotation(event)
 {
-	if (event.buttons & 1 != 0) {
+	if (event.type === "mousemove" && event.buttons & 1 != 0) {
 		rot_field_XYZ_onZ(event.clientX - prev_clientX, event.clientY - prev_clientY);
 		prev_clientX = event.clientX;
 		prev_clientY = event.clientY;
+	} else if (event.type === "touchmove" && event.touches.length == 1) {
+		rot_field_XYZ_onZ(event.touches[0].clientX - prev_clientX, event.touches[0].clientY - prev_clientY);
+		prev_clientX = event.touches[0].clientX;
+		prev_clientY = event.touches[0].clientY;
 	}
 }
 
